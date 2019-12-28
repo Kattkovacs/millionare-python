6a1930c5ec21c390dded249f50a9aec5bf31c45b
sztgellert <sztgellert@hotmail.com>
2019-12-28 18:25:09 +0100

    sound added

--- millonaire.py
+++ millonaire.py
@@ -4,10 +4,16 @@ import sys
 import os
 from sty import Style, RgbFg, fg, rs
 import time
+from playsound import playsound
+import pygame
 
 
 def game_start():
     fg.purple = Style(RgbFg(148, 0, 211))
+    pygame.mixer.init()
+    os.system('clear')
+    pygame.mixer.music.load("loim_intro.wav")
+    pygame.mixer.music.play()
     print("This is the game of games..\nIn the arena..\nMr Steven Vágó is awating You!\n"+fg.purple+"Become the next Millionaire!\n"+fg.rs)
 
 def quiz():
@@ -58,11 +64,13 @@ def quiz():
             print(" |"+ first_choice + (" "*(table_line_length-(len(shuffled_line[0])+len(shuffled_line[1])+9))) + second_choice + " |")
             print(" |"+ third_choice + (" "*(table_line_length-(len(shuffled_line[2])+len(shuffled_line[3])+9)))+fourth_choice + " |")
             print(" "+"-"*table_line_length)          
-            answer = input("Select the correct answer(a,b,c,d): \n(In case you need help type 'h')")
             counter += 1
-            if answer=='h':
+            answer = input("Select the correct answer(a,b,c,d): \n(In case you need help type 'h')")
+            if answer.lower()=='h':
                 help_=input("For Audience's help, type 'a'\nFor Telephone help type 't'\nFor halving type 'h': ")
-                if help_=="a":
+                if help_.lower()=="a":
+                    pygame.mixer.music.load("kozonseg.wav")
+                    pygame.mixer.music.play()
                     for choise in [a,b,c,d]:
                         if choise ==current_line[0]:
                             if choise==a:
@@ -95,53 +103,112 @@ def quiz():
                             print(f'C: {c_percent}%')
                             time.sleep(1)
                             print(f'D: {d_percent}%')
-                    answer = input("Select the correct answer(a,b,c,d): \n(In case you need help type 'h')")
-                if help_=="t":
+                            pygame.mixer.fadeout(2)
+                if help_.lower()=="t":
                     phone=input("Who'd you like to call?\nfor mum, press 'm'\nfor dad press 'd'\nfor old teacher from high school press 't'\nfor Maester Yoda press 'y': ")
-                    if phone=='m':
+                    if phone.lower()=='m':
+                        pygame.mixer.music.load("telhiv.wav")
+                        pygame.mixer.music.play()
+                        pygame.mixer.music.load("telefon.wav")
+                        pygame.mixer.music.play()
+                        then = time.time()
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         print("Hi mummy, I'm playing the Millionaire..Here's the question.. \n"+', '.join(question_table[i]))
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("Hi Honey.. I don't know the answer i'll ask your dad")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("..Dad!..Daddy!..Our son is in the Millionaire Show!! Can you believe??")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print(fg.red+"Mum please my time is almost up!!"+fg.rs)
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("Okay honey, it is D.. No, no wait, it is " + current_line[0])
-                    if phone=='d':
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
+                        now = time.time()
+                        pygame.mixer.fadeout(2)
+                        print("Call Duration: ", int(now-then), " seconds\ 30 seconds")
+                    if phone.lower()=='d':
+                        pygame.mixer.music.load("telhiv.wav")
+                        pygame.mixer.music.play()
+                        pygame.mixer.music.load("telefon.wav")
+                        pygame.mixer.music.play()
+                        then = time.time()
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         print("Hi dad, I'm playing the Millionaire..Here's the question.. \n"', '.join(question_table[i]))
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("Hi Son.. I don't know the answer i'll ask your grandfather")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("..Dad!..Daddy!..Your grandson is in the Millionaire Show!! Can you believe??")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print(fg.red+"Dad please my time is almost up!!"+fg.rs)
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("Okay Son, he says it is A.. No, no wait, it is " + current_line[0])
-                    if phone=='t':
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
+                        now = time.time()
+                        pygame.mixer.fadeout(2)
+                        print("Call Duration: ", int(now-then), " seconds\ 30 seconds")
+                    if phone.lower()=='t':
+                        pygame.mixer.music.load("telhiv.wav")
+                        pygame.mixer.music.play()
+                        pygame.mixer.music.load("telefon.wav")
+                        pygame.mixer.music.play()
+                        then = time.time()
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         print("Great day Mr Teacher I am your former student, and I'm playing the Millionaire..Here's the question..\n"', '.join(question_table[i]))
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("Welcome.. I'd never thought after so many years you'd call me!")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("I have to tell the principle..Mr!..Mr Principle!..One of your former student is in the Millionaire Show!! Can you believe??")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print(fg.red+"Sir please my time is almost up!!"+fg.rs)
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("Okay boy, he says it is A.. No, no wait, it is " + current_line[0])
-                    if phone=='y':
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
+                        now = time.time()
+                        pygame.mixer.fadeout(2)
+                        print("Call Duration: ", int(now-then), " seconds\ 30 seconds")
+                    if phone.lower()=='y':
+                        pygame.mixer.music.load("telhiv.wav")
+                        pygame.mixer.music.play()
+                        pygame.mixer.music.load("telefon.wav")
+                        pygame.mixer.music.play()
+                        then = time.time()
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
+                        #Time after it finished
                         print("May the force be with you, my son. How can I help u?")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("Hi Yoda, I need your force of knowledge because I'm playing the Millionaire..Here's the question..\n"', '.join(question_table[i]))
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("I call the Force for help")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("*background* Hi Leila I'm Yoda please help this is the question..")
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
                         time.sleep(2)
                         print("The Force says "+ current_line[0] +"\nNever forget, do or don't but never try!")
-                    answer = input("Select the correct answer(a,b,c,d): \n(In case you need help type 'h')")
-                if help_=="h":
+                        print(fg.orange+str(30-int(time.time()-then))+fg.rs)
+                        now = time.time()
+                        pygame.mixer.fadeout(2)
+                        print("Call Duration: ", int(now-then), " seconds\ 30 seconds max.")
+                    answer = input("Select the correct answer(a,b,c,d):")
+                if help_.lower()=="h":
                     os.system('clear')
                     time.sleep(2)
+                    pygame.mixer.music.load("felezo.wav")
+                    pygame.mixer.music.play()
                     for head_lines in range(len(vago_feje_sorai)):
                         print(''.join(vago_feje[head_lines]))
                     print(" "+"-"*(table_line_length))
@@ -171,22 +238,27 @@ def quiz():
                     print(" |"+ first_choice + (" "*(table_line_length-(len(shuffled_line[0])+len(shuffled_line[1])+9))) + second_choice + " |")
                     print(" |"+ third_choice + (" "*(table_line_length-(len(shuffled_line[2])+len(shuffled_line[3])+9)))+fourth_choice + " |")
                     print(" "+"-"*table_line_length)          
-                    answer = input("Select the correct answer(a,b,c,d): \n(In case you need help type 'h')")
+                answer = input("Select the correct answer(a,b,c,d): \n(In case you need help type 'h')")
             answer = input("Are you sure?")
-            if answer == 'a':
+            if answer.lower()=='a':
                 answer = a
-            if answer == 'b':
+            if answer.lower()=='b':
                 answer = b
-            if answer == 'c':
+            if answer.lower()== 'c':
                 answer = c
-            if answer == 'd':
+            if answer.lower()== 'd':
                 answer = d
             if answer == current_line[0]:
+                pygame.mixer.music.load("jo valasz.wav")
+                pygame.mixer.music.play()
                 fg.green = Style(RgbFg(0, 255, 0))
                 print(fg.green + "Well done!" + fg.rs)
                 time.sleep(2)
                 os.system('clear')
             else:
+                pygame.mixer.music.load("rossz valasz.wav")
+                pygame.mixer.music.play()
+                time.sleep(2)
                 fg.red = Style(RgbFg(255, 0, 0))
                 print(fg.red+answer+"\nBetter luck next time!"+fg.rs)
                 sys.exit(0)
