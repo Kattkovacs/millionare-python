1fd055ab8cb8feff32955e030ba31516c330ce33
sztgellert <sztgellert@hotmail.com>
2020-01-20 16:51:18 +0100

    added 62 new questions from the Millionaire Show and also removed 2 which are basically bad. Now the game always uses random questions selected from 5 consecutive questions

---
+++ prices_try.py
@@ -0,0 +1,7 @@
+import random
+mylist=["a","b","c","d"]
+print(len("                                                                             "))
+mylist=mylist[1:4]
+print(mylist)
+al=random.choice(mylist[0:3])
+print(al)
