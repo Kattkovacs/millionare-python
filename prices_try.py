9f894f601b9adc2ae47176735cf5041f64595918
sztgellert <sztgellert@hotmail.com>
2020-02-02 22:29:55 +0100

    audience's help updated: new animation+window

--- prices_try.py
+++
@@ -1,7 +0,0 @@
-import random
-mylist=["a","b","c","d"]
-print(len("                                                                             "))
-mylist=mylist[1:4]
-print(mylist)
-al=random.choice(mylist[0:3])
-print(al)
